d303a89943b842429b13014ea09399d9ec4b1a09
ricardoalmeida <ricardoluiz1.almeida@gmail.com>
2025-07-12 15:01:03 +0200

    Install global packages

--- macos/brew.sh
+++ macos/brew.sh
@@ -5,3 +5,7 @@ brew upgrade
 # install all dependencies contained in ./Brewfile
 echo "[brew] installing the casks..."
 brew bundle
+
+# install global npm packages
+echo "[npm] installing global packages..."
+npm install -g @anthropic-ai/claude-code
